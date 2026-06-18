cb6c62c26a1a29a5e5f12cee64c6f70d6cc24218
PenuelDaimari <phd0692@gmail.com>
2026-06-19 01:12:37 +0530

    fixed backend deployment issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: config.FRONTEND_URL,
+    origin: config.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
